cdcabf5602bab2c8d0c0a119b71b56111848d0df
Stuart R. Slattery <sslattery617@gmail.com>
2014-06-24 15:28:36 -0400

    Adding mc_data.py for construction of Monte Carlo linear solver data structures.

---
+++ notebooks/stuart/mc_data.py
@@ -0,0 +1,89 @@
+##---------------------------------------------------------------------------##
+# mc_data.py
+# Data structure functions for Monte Carlo calculations
+##---------------------------------------------------------------------------##
+import numpy
+
+##---------------------------------------------------------------------------##
+## Make a Poisson operator with cell centered differences.
+##---------------------------------------------------------------------------##
+def makePoissonOperator( grid_size ):
+    A = numpy.zeros((grid_size,grid_size))
+    A[0][0] = 1.0
+    A[0][1] = -1.0/3.0
+    for i in xrange(1,grid_size-1):
+        A[i][i-1] = -1.0/2.0
+        A[i][i] = 1.0
+        A[i][i+1] = -1.0/2.0
+        A[grid_size-1][grid_size-2] = -1.0/3.0
+        A[grid_size-1][grid_size-1] = 1.0
+    return A
+
+##---------------------------------------------------------------------------##
+## Make an iteration matrix H = I-A for a given operator
+##---------------------------------------------------------------------------##
+def makeIterationMatrix( A ):
+    grid_size = len(A)
+    H = numpy.zeros((grid_size,grid_size))
+    for i in xrange(grid_size):
+        for j in xrange(grid_size):
+            H[i][j] = -A[i][j]
+            if i == j:
+                H[i][j] = H[i][j] + 1.0
+    return H
+
+##---------------------------------------------------------------------------##
+## Make an adjoint probability matrix for a given iteration matrix.
+##---------------------------------------------------------------------------##
+def makeProbabilityMatrix( H ):
+    grid_size = len(H)
+    P = numpy.zeros((grid_size,grid_size))
+    for i in xrange(grid_size):
+        row_sum = 0
+        for j in xrange(grid_size):
+            row_sum = row_sum + abs(H[j][i])
+        for j in xrange(grid_size):
+            P[i][j] = abs(H[j][i])/row_sum
+        for j in xrange(grid_size):
+            P[i][j] = P[i][j] / P[i][grid_size-1]
+    return P
+
+##---------------------------------------------------------------------------##
+## Make a cumulative distribution matrix for a given probability matrix.
+##---------------------------------------------------------------------------##
+def makeCDFMatrix( P ):
+    grid_size = len(P)
+    C = numpy.zeros((grid_size,grid_size))
+    for i in xrange(grid_size):
+        C[i][0] = P[i][0]
+        for j in xrange(1,grid_size):
+            C[i][j] = C[i][j-1] + P[i][j]
+    return C
+
+##---------------------------------------------------------------------------##
+## Make a weight matrix from an iteration matrix and a probability
+## matrix.
+## ---------------------------------------------------------------------------##
+def makeWeightMatrix( H, P ):
+    grid_size = len(H)
+    W = numpy.zeros((grid_size,grid_size))
+    for i in xrange(grid_size):
+        for j in xrange(grid_size):
+            W[i][j] = H[i][j] / P[i][j]
+    return W
+
+##---------------------------------------------------------------------------##
+## Make H, C, and W for a Monte Carlo problem.
+##---------------------------------------------------------------------------##
+def makeMonteCarloHCW( A ):
+    H = makeIterationMatrix( A )
+    P = makeProbabilityMatrix( H )
+    C = makeCDFMatrix( P )
+    W = makeWeightMatrix( H, P )
+    return (H, C, W)
+
+##---------------------------------------------------------------------------##
+## end mc_data.py
+##---------------------------------------------------------------------------##
+
+
